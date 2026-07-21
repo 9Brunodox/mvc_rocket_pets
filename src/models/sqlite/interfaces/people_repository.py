46544ed2961fc665db5c0669cc3fae1b924227d3
9Brunodox <bruno.moreiradasilva.2002@gmail.com>
2026-07-21 00:06:14 -0300

    improvement: Addinmg interfaces in repositotories

---
+++ src/models/sqlite/interfaces/people_repository.py
@@ -0,0 +1,22 @@
+from typing import List
+from abc import ABC, abstractmethod
+
+from src.models.sqlite.entities.people import PeopleTable
+
+class PeopleRepositoryInterface(ABC):
+
+    @abstractmethod
+    def insert_person(self, first_name:str, last_name: str, age: int, pet_id: int) -> None:
+        pass
+
+    @abstractmethod
+    def list_peoples(self) -> List:
+        pass
+
+    @abstractmethod
+    def delete_person(self, name: str) -> None:
+        pass
+
+    @abstractmethod
+    def get_person(self, person_id: int) -> PeopleTable:
+        pass
--- src/models/sqlite/repositories/pets_repository.py
+++ src/models/sqlite/repositories/pets_repository.py
@@ -1,12 +1,13 @@
 from typing import List # Para python 3.8 ou inferior
 from sqlalchemy.orm.exc import NoResultFound
 from src.models.sqlite.entities.pets import PetsTable
+from src.models.sqlite.interfaces.pets_repository import PetsRepositoryInterface
 
-class PetsRepository:
+class PetsRepository(PetsRepositoryInterface):
     def __init__(self, db_connection) -> None:
         self.db_connection = db_connection
 
-    def list_pets(self) -> List:
+    def list_pets(self) -> List[PetsTable]:
         with self.db_connection as db:
             try:
                 pets = db.session.query(PetsTable).all()
